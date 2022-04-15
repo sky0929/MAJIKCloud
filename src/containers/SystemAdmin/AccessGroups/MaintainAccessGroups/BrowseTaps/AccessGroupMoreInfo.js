0444c1a934d9e90ee0e15c76cc5da8012f5417c5
Ace Hood <acehood010126@gmail.com>
2022-04-15 01:04:36 -0700

    Access Group More Info

--- src/containers/SystemAdmin/AccessGroups/MaintainAccessGroups/BrowseTaps/AccessGroupMoreInfo.js
+++ src/containers/SystemAdmin/AccessGroups/MaintainAccessGroups/BrowseTaps/AccessGroupMoreInfo.js
@@ -1,52 +1,13 @@
-import { TextField } from "@mui/material";
-import { Box, Button, Stack } from "@mui/material";
+import { Button, Stack, Grid } from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import ReactGrid from "../../../../../components/ReactGrid";
 import React, { useState } from "react";
 
 const AccessGroupMoreInfo = (props) => {
-  const columns = [
-    {
-      name: "id",
-      header: "Id",
-      defaultVisible: false,
-      defaultWidth: 60,
-      type: "number",
-    },
-    { name: "queue", header: "Queue", defaultFlex: 1 },
-    { name: "description", header: "Description", defaultFlex: 3 },
-    { name: "statusflag", header: "Status Flag", defaultFlex: 1 },
-    { name: "status", header: "Status", defaultFlex: 1 },
-  ];
-
-  const data = [
-    {
-      id: 1,
-      queue: "EOM",
-      description: "End of Month Reports",
-      statusflag: "",
-      status: "Unused",
-    },
-    {
-      id: 2,
-      queue: "MAIN",
-      description: "EoM Job Queue",
-      statusflag: "",
-      status: "Unused",
-    },
-  ];
-
-  const onSelectionChange = (selectedRow) => {
-    console.log(selectedRow);
-  };
-
   const [fullWidth, setFullWidth] = useState(true);
-  const [maxWidth, setMaxWidth] = useState("sm");
-
-  const [value, setValue] = React.useState("1");
+  const [maxWidth, setMaxWidth] = useState("lg");
 
   return (
     <Dialog
@@ -55,22 +16,27 @@ const AccessGroupMoreInfo = (props) => {
       open={props.show}
       onClose={props.handleClose}
     >
-      <DialogTitle>SYPRFB: Browse Batch Queues</DialogTitle>
+      <DialogTitle>Access Group More Info</DialogTitle>
       <DialogContent>
-        <Box sx={{ mb: 1 }}>
-          <ReactGrid
-            columns={columns}
-            data={data}
-            selectedRow={(row) => {
-              console.log(row);
-            }}
-          />
-        </Box>
+        <Grid container direction="row" spacing={0} justifyContent="left">
+          <Grid item xs={4}>
+            ADMIN
+          </Grid>
+          <Grid item xs={8}>
+            System Administrator
+          </Grid>
+        </Grid>
+        <Grid container direction="row" spacing={0} justifyContent="left">
+          <Grid item xs={4}>
+            Property
+          </Grid>
+          <Grid item xs={8}>
+            Site Services
+          </Grid>
+        </Grid>
         <DialogActions>
           <Stack direction="row" spacing={2}>
-            <Button variant="contained">Select</Button>
-            <Button variant="contained">Create</Button>
-            <Button variant="contained">Modify</Button>
+            <Button variant="contained">Next Page</Button>
             <Button variant="contained" onClick={props.handleClose}>
               Close
             </Button>
